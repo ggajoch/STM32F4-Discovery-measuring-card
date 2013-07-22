98100f1f0c84b5fdbb4c13c8bcf43de7ccf8600e
U-ZYLA\lab <lab@Zyla.(none)>
2013-07-22 15:20:58 +0200

    .gitignore is now working

--- Codes/STM32F4xx_Ethernet_Example/Project/FreeRTOS/httpserver_netconn/MDK-ARM/Discover-More/inet.d
+++
@@ -1,9 +0,0 @@
-.\discover-more\inet.o: ..\..\..\..\Utilities\lwip_v1.3.2\src\core\ipv4\inet.c
-.\discover-more\inet.o: ..\..\..\..\Utilities\lwip_v1.3.2\src\include\lwip/opt.h
-.\discover-more\inet.o: ..\inc\lwipopts.h
-.\discover-more\inet.o: ..\..\..\..\Utilities\lwip_v1.3.2\src\include\lwip/debug.h
-.\discover-more\inet.o: ..\..\..\..\Utilities\lwip_v1.3.2\src\include\lwip/arch.h
-.\discover-more\inet.o: ..\..\..\..\Utilities\lwip_v1.3.2\port\STM32F4x7\arch/cc.h
-.\discover-more\inet.o: ..\..\..\..\Utilities\lwip_v1.3.2\port\STM32F4x7\arch/cpu.h
-.\discover-more\inet.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
-.\discover-more\inet.o: ..\..\..\..\Utilities\lwip_v1.3.2\src\include\ipv4\lwip/inet.h
